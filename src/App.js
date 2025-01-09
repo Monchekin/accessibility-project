6469ca5b02b83bda9043e386c278c360a77be8e5
Lina <lina.barthelson@gmail.com>
2025-01-09 11:24:43 +0100

    Update 3 for Github Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import HomePage from './pages/HomePage';
 import Buttons from './pages/ButtonPage';
@@ -16,25 +16,19 @@ const App = () => {
   }, []);
 
   return (
-    <>
-      <div className='appContentWrap'>
+    <Router basename="/accessibility-project">
+      <div className="appContentWrap">
         <header>
           <Navbar />
         </header>
         <Routes>
-          <Route path='/accessibility-project/' element={<HomePage />} />
-          <Route path='/accessibility-project/buttons' element={<Buttons />} />
-          <Route
-            path='/accessibility-project/inlineInput'
-            element={<InlineInput />}
-          />
-          <Route
-            path='/accessibility-project/OnTopInputs'
-            element={<OnTopInput />}
-          />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/buttons" element={<Buttons />} />
+          <Route path="/inlineInput" element={<InlineInput />} />
+          <Route path="/OnTopInputs" element={<OnTopInput />} />
         </Routes>
       </div>
-    </>
+    </Router>
   );
 };
 
